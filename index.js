108e1e2f1671282c0f7221f5aa161d37ac1ae93b
TatianaChF <tatiana.chefox@gmail.com>
2025-05-28 20:38:07 +0300

    adding input

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const circle = document.getElementById('progress');
 const radius = circle.r.baseVal.value;
 const circumference = 2 * Math.PI * radius;
+const input = document.getElementById("percent");
 
 circle.style.strokeDasharray = `${circumference}`;
 circle.style.strokeDashoffset = circumference;
