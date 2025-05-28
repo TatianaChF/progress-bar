ded0a5f1cc403ab6eb201a912af1d83a515d58fc
TatianaChF <tatiana.chefox@gmail.com>
2025-05-28 20:35:13 +0300

    getting element by id

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const circle = document.querySelector(".progress-ring__circle");
+const circle = document.getElementById('progress');
 const radius = circle.r.baseVal.value;
 const circumference = 2 * Math.PI * radius;
 
-circle.style.strokeDasharray = `${circumference} ${circumference}`;
-circle.style.strokeDashoffset = `${circumference}`;
+circle.style.strokeDasharray = `${circumference}`;
+circle.style.strokeDashoffset = circumference;
 
 function setProgress(percent) {
-    const offset = circumference - percent / 100 * circumference;
+    const offset = circumference - (percent / 100) * circumference;
     circle.style.strokeDashoffset = offset;
 }
+
+setProgress(70);
