3c03ab0b26f4ebe65aae10fcb91e561528440ac1
TatianaChF <tatiana.chefox@gmail.com>
2025-05-28 20:03:18 +0300

    definition of strokeDasharray and strokeDashoffset in js file

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const circle = document.querySelector(".progress-ring__circle");
 const radius = circle.r.baseVal.value;
 const circumference = 2 * Math.PI * radius;
 
+circle.style.strokeDasharray = `${circumference} ${circumference}`;
+circle.style.strokeDashoffset = `${circumference}`;
+
 function setProgress(percent) {
     const offset = circumference - percent / 100 * circumference;
     circle.style.strokeDashoffset = offset;
