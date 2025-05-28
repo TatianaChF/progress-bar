64d2139593b8ed721a3e3f1950b2339307d1a7e1
TatianaChF <tatiana.chefox@gmail.com>
2025-05-28 19:41:21 +0300

    adding a function to change progress

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ const circle = document.querySelector(".progress-ring__circle");
 const radius = circle.r.baseVal.value;
 const circumference = 2 * Math.PI * radius;
 
-console.log(circumference);
+function setProgress(percent) {
+    const offset = circumference - percent / 100 * circumference;
+    circle.style.strokeDashoffset = offset;
+}
