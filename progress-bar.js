a833bcdd3bd906f041f39532690ed346a9d3febf
TatianaChF <tatiana.chefox@gmail.com>
2025-05-29 21:46:11 +0300

    adding a function for initialization with the creation of a structure

--- progress-bar.js
+++ progress-bar.js
@@ -12,5 +12,59 @@ class ProgressBar {
 
         this.config = {...this.defaults, ...options};
         this.container = container;
+
+        this.init();
+    }
+
+    init() {
+        this.container.innerHTML = `
+            <div class="container">
+                <div id="progress-container" ${this.config.hidden ? 'style="display:none"' : ''}>
+                    <svg width="${this.config.size}"
+                        height="${this.config.size}"
+                        viewBox="0 0 ${this.config.size} ${this.config.size}">
+                        <circle class="progress-bg"
+                                cx="${this.config.size/2}"
+                                cy="${this.config.size/2}"
+                                r="${this.config.size/2 - this.config.strokeWidth}" />
+                        <circle class="progress-bar"
+                                cx="${this.config.size/2}"
+                                cy="${this.config.size/2}"
+                                r="${this.config.size/2 - this.config.strokeWidth}"
+                                id="progress" />
+                    </svg>
+                </div>
+                <div class="settings-container">
+                    <div class="input-container">
+                        <input type="number"
+                               class="percent"
+                               id="percent"
+                               value="${this.config.value}"
+                               name="percent"
+                               min="0"
+                               max="100" />
+                        <label for="percent">Value</label>
+                    </div>
+                <div class="switch-container">
+                    <label class="switch">
+                        <input type="checkbox" 
+                               id="animate-toggle" 
+                               ${this.config.animated ? 'checked' : ''} />
+                        <span class="slider"></span>
+                    </label>
+                    <span>Animate</span>
+                </div>
+                <div class="switch-container">
+                    <label class="switch">
+                        <input type="checkbox" 
+                               id="hide-toggle" 
+                               ${this.config.hidden ? 'checked' : ''} />
+                        <span class="slider"></span>
+                    </label>
+                    <span class="switch-label">Hide</span>
+                </div>
+            </div>
+        </div>
+    `
     }
 }
