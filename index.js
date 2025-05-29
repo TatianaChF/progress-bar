d7c3eae4a4f92420146ae277d58edb2d7994d18e
TatianaChF <tatiana.chefox@gmail.com>
2025-05-29 12:01:44 +0300

    adding an event handler for animation

--- index.js
+++ index.js
@@ -21,6 +21,14 @@ input.addEventListener("change", () => {
     setProgress(value);
 })
 
+animateToggle.addEventListener("change", () => {
+    if (animateToggle.checked) {
+        circle.classList.add("animated");
+    } else {
+        circle.classList.remove("animated");
+    }
+})
+
 function setProgress(percent) {
     const offset = circumference - (percent / 100) * circumference;
     circle.style.strokeDashoffset = offset;
