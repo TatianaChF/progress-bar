9bbaea7f58e3e6e6db3d50835b180bf1ec43a4d8
TatianaChF <tatiana.chefox@gmail.com>
2025-05-30 15:26:58 +0300

    add comments

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+/**
+ * Инициализация прогресс-бара
+ *
+ * Создаем экземпляр ProgressBar с параметрами:
+ * - Размер: 170px
+ * - Толщина линии: 10px
+ * - Начальное значение: 0
+ * - Анимация отключена
+ * - Компонент видим
+ */
 const progressBar = new ProgressBar(document.getElementById('progress-bar'), {
     size: 170,
     strokeWidth: 10,
