644021e5ad6ebaf1a9b3ef6b580317293e063cc0
TatianaChF <tatiana.chefox@gmail.com>
2025-05-30 15:00:58 +0300

    change class

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const progressBar = new ProgressBar(document.getElementById('progress-bar'), {
     size: 170,
     strokeWidth: 10,
-    bgColor: '#f0f0f0',
-    progressColor: '#2196F3',
     value: 0,
     animated: false,
     hidden: false
--- progress-bar.js
+++ progress-bar.js
@@ -3,8 +3,6 @@ class ProgressBar {
         this.defaults = {
             size: 170,
             strokeWidth: 10,
-            bgColor: '#f0f0f0',
-            progressColor: 'blue',
             value: 0,
             animated: false,
             hidden: false
